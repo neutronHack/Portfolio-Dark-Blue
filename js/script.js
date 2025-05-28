28c44c0f93de33e0bd153afe9410f0f0ba06672a
jimmy <jimmycabalz.a.2011@gmail.com>
2025-05-28 01:31:09 -0600

    Subiendo portfolio1 con archivos multimedia, documentacion y codigo

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//Funcion para contactar. Click al boton "Contáctame" y redirigir a correo electrónico
+document.getElementById("btn-contactame").addEventListener("click", function(e) {
+    setTimeout(function() {
+        window.location.href = "mailto:jimmycabalz.a.2011@gmail.com";
+    }, 1000);
+});
+
+//Funcion para contactar. Click al icono de Contacto al final de la pagina y redirigir a correo electrónico
+document.getElementById("segundoenvioemail").addEventListener("click", function(e) {
+    setTimeout(function() {
+        window.location.href = "mailto:jimmycabalz.a.2011@gmail.com";
+    }, 1000);
+});
+
